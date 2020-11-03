6fd70eed36818e37517254683e0cfaca266442ca
Jim Cooper <jim@cooperplanet.com>
2020-11-02 21:07:04 -0700

    Reduce # of hardcoded cart items

--- src/cart/Cart.vue
+++ src/cart/Cart.vue
@@ -38,27 +38,6 @@ const products = [{
   category: 'Kayaking',
   description: 'Carbon fiber body with platinum hooks make this the perfect gift for that special someone.',
   image: 'ultimate-dreamline-kayak.jpg',
-}, {
-  id: '6130cdd4-071a-4559-8072-35f0fbec5516',
-  title: 'Moccasins',
-  price: 35.95,
-  category: 'Climbing',
-  description: 'Ditch the heavy boots for these lightweight hiking sandals. Perfect for rugged terrain and hiking in streams.',
-  image: 'moccasins.jpg',
-}, {
-  id: 'dd0343e9-50b2-4f1d-8b87-93c0b34f3d35',
-  title: 'Firebrand Kayak',
-  price: 499.95,
-  category: 'Kayaking',
-  description: 'Why blend in when you can scream speed with this hot and sleek firebrand?',
-  image: 'firebrand-kayak.jpg',
-}, {
-  id: '7277956e-795f-4c0f-9861-cf03635df5ea',
-  title: 'Mega Pokey Kit',
-  price: 129.99,
-  category: 'Climbing',
-  description: 'Everything you need for traction on that thrilling ice climb.',
-  image: 'mega-pokey-kit.jpg',
 }];
 
 export default {
